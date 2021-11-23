7b09042ad22fbede8a9331eb24a2623bd33fcccd
keqing555 <1845441591@qq.com>
2021-11-23 20:35:17 +0800

    merge;compute

--- src/com/jdk/demo/Map_merge.java
+++ src/com/jdk/demo/Map_merge.java
@@ -27,12 +27,24 @@ public class Map_merge {
         System.out.println(scoreMap);
 
         //merge方法
-        Map<String,Integer> scoreMap2=new HashMap<>();
-        stuList.forEach(stu->{
-            scoreMap2.merge(stu.getName(),stu.getScore(),Integer::sum);
+        Map<String, Integer> scoreMap2 = new HashMap<>();
+        stuList.forEach(stu -> {
+            scoreMap2.merge(stu.getName(), stu.getScore(), Integer::sum);
         });
         System.out.println(scoreMap2);
 
-
+        //compute 获取map指定key的value进行操作，ket不存在也会插入
+        Map<String, Integer> scoreMap3 = new HashMap<>();
+        stuList.forEach(stu -> {
+            scoreMap3.compute(stu.getName(), (k, v) -> {
+                if (v == null) {
+                    v = stu.getScore();
+                } else {
+                    v += stu.getScore();
+                }
+                return v;
+            });
+        });
+        System.out.println(scoreMap3);
     }
 }
