28bbe01ae7eb307b462d4bcff6a8c79db0633a11
pengbangchang <1845441591@qq.com>
2021-12-30 10:29:51 +0800

    增加左移、右移

---
+++ src/com/jdk/demo/Operator.java
@@ -0,0 +1,14 @@
+package com.jdk.demo;
+
+public class Operator {
+
+    public static void main(String[] args) {
+        //左移右移
+        int a = 9;
+        int b = -9;
+        System.out.println(a >> 2);
+        System.out.println(a << 2);
+        System.out.println(b >> 2);
+        System.out.println(b << 2);
+    }
+}
