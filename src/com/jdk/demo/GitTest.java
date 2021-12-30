dceafc90f96dc291ceea5841c37d94dc4c2896af
keqing <1845441591@qq.com>
2021-12-31 00:46:55 +0800

    up

--- src/com/jdk/demo/GitTest.java
+++ src/com/jdk/demo/GitTest.java
@@ -5,10 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("a test");
         System.out.println("nothing");
-<<<<<<< HEAD
-=======
-        System.out.println("a");
-        System.out.println("b");
->>>>>>> 3eb5a7cbae49cf4db1050002d8ac05c95853ac1a
+        System.out.println("11111");
     }
 }
